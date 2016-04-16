a79ac9468e5be7a9bf87605fd207a56fff23d7c8
bmalinowsky <bmalinowsky@github.com>
2016-04-16 15:37:42 +0200

    Add test network

---
+++ src/main/java/tuwien/auto/calimero/TestDeviceLogic.java
@@ -0,0 +1,469 @@
+/*
+    Calimero 2 - A library for KNX network access
+    Copyright (c) 2010, 2016 B. Malinowsky
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Linking this library statically or dynamically with other modules is
+    making a combined work based on this library. Thus, the terms and
+    conditions of the GNU General Public License cover the whole
+    combination.
+
+    As a special exception, the copyright holders of this library give you
+    permission to link this library with independent modules to produce an
+    executable, regardless of the license terms of these independent
+    modules, and to copy and distribute the resulting executable under terms
+    of your choice, provided that you also meet, for each linked independent
+    module, the terms and conditions of the license of that module. An
+    independent module is a module which is not derived from or based on
+    this library. If you modify this library, you may extend this exception
+    to your version of the library, but you are not obligated to do so. If
+    you do not wish to do so, delete this exception statement from your
+    version.
+*/
+
+package tuwien.auto.calimero;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import tuwien.auto.calimero.datapoint.Datapoint;
+import tuwien.auto.calimero.datapoint.DatapointMap;
+import tuwien.auto.calimero.datapoint.StateDP;
+import tuwien.auto.calimero.device.KnxDevice;
+import tuwien.auto.calimero.device.KnxDeviceServiceLogic;
+import tuwien.auto.calimero.device.ServiceResult;
+import tuwien.auto.calimero.device.ios.InterfaceObject;
+import tuwien.auto.calimero.device.ios.InterfaceObjectServer;
+import tuwien.auto.calimero.device.ios.KNXPropertyException;
+import tuwien.auto.calimero.dptxlator.DPTXlator;
+import tuwien.auto.calimero.dptxlator.DPTXlator2ByteFloat;
+import tuwien.auto.calimero.dptxlator.DPTXlator2ByteUnsigned;
+import tuwien.auto.calimero.dptxlator.DPTXlator3BitControlled;
+import tuwien.auto.calimero.dptxlator.DPTXlator4ByteFloat;
+import tuwien.auto.calimero.dptxlator.DPTXlator8BitUnsigned;
+import tuwien.auto.calimero.dptxlator.DPTXlatorBoolean;
+import tuwien.auto.calimero.dptxlator.DPTXlatorString;
+import tuwien.auto.calimero.dptxlator.TranslatorTypes;
+import tuwien.auto.calimero.knxnetip.Discoverer;
+import tuwien.auto.calimero.link.KNXLinkClosedException;
+import tuwien.auto.calimero.link.KNXNetworkLink;
+import tuwien.auto.calimero.mgmt.Description;
+import tuwien.auto.calimero.mgmt.Destination;
+import tuwien.auto.calimero.mgmt.LinkProcedure;
+import tuwien.auto.calimero.mgmt.ManagementClientImpl;
+import tuwien.auto.calimero.mgmt.PropertyAccess;
+import tuwien.auto.calimero.mgmt.PropertyAccess.PID;
+import tuwien.auto.calimero.mgmt.TransportLayer;
+import tuwien.auto.calimero.server.knxnetip.RoutingEndpoint;
+
+/**
+ * Test device logic for KNX devices in our test network.
+ */
+class TestDeviceLogic extends KnxDeviceServiceLogic
+{
+	// PID.PROJECT_INSTALLATION_ID
+	private static final int defProjectInstallationId = 0;
+	// PID.ROUTING_MULTICAST_ADDRESS
+	private static final InetAddress defRoutingMulticast;
+	// PID.MAC_ADDRESS
+	private static final byte[] defMacAddress;
+
+	static {
+		InetAddress a = null;
+		try {
+			a = InetAddress.getByName(Discoverer.SEARCH_MULTICAST);
+		}
+		catch (final UnknownHostException e) {}
+		defRoutingMulticast = a;
+
+		byte[] mac = new byte[6];
+		try {
+			mac = NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress();
+		}
+		catch (final SocketException | UnknownHostException e) {}
+		defMacAddress = mac;
+	}
+
+	// Values used for service families DIB
+
+	// PID.KNXNETIP_DEVICE_CAPABILITIES
+	// Bits LSB to MSB: 0 Device Management, 1 Tunneling, 2 Routing, 3 Remote Logging,
+	// 4 Remote Configuration and Diagnosis, 5 Object Server
+	private static final int defDeviceCaps = 1 + 2 + 4;
+
+	// server friendly name, matches PID.FRIENDLY_NAME property
+	private static final String friendlyName = "KNX Test Device";
+
+	private final Map<GroupAddress, String> state = new HashMap<>();
+	private final List<Datapoint> isResponder = new ArrayList<>();
+
+	TestDeviceLogic() throws KNXException
+	{
+		final List<StateDP> l = new ArrayList<StateDP>();
+		l.add(new StateDP(new GroupAddress("1/0/1"), "Bool", 0, DPTXlatorBoolean.DPT_BOOL.getID()));
+		l.add(new StateDP(new GroupAddress("1/0/11"), "Bool 2", 0, DPTXlatorBoolean.DPT_ENABLE.getID()));
+		l.add(new StateDP(new GroupAddress("1/0/111"), "Bool 3", 0, DPTXlatorBoolean.DPT_OCCUPANCY.getID()));
+		l.add(new StateDP(new GroupAddress("1/0/2"), "", 0, DPTXlator3BitControlled.DPT_CONTROL_BLINDS.getID()));
+		l.add(new StateDP(new GroupAddress("1/0/3"), "", 0, DPTXlator8BitUnsigned.DPT_SCALING.getID()));
+		l.add(new StateDP(new GroupAddress("1/0/4"), "", 0, DPTXlator2ByteUnsigned.DPT_VALUE_2_UCOUNT.getID()));
+		l.add(new StateDP(new GroupAddress("1/0/5"), "", 0, DPTXlatorString.DPT_STRING_8859_1.getID()));
+		l.add(new StateDP(new GroupAddress("1/0/6"), "", 0, DPTXlator2ByteFloat.DPT_RAIN_AMOUNT.getID()));
+		l.add(new StateDP(new GroupAddress("1/0/7"), "", 0, DPTXlator4ByteFloat.DPT_ACCELERATION.getID()));
+
+		for (final StateDP dp : l) {
+			getDatapointModel().add(dp);
+			final String s = TranslatorTypes.createTranslator(0, dp.getDPT()).getValue();
+			state.put(dp.getMainAddress(), s);
+		}
+		state.put(new GroupAddress("1/0/5"), "Hello KNX!");
+
+		for (int i = 0; i < 5000; i++)
+			writeMemory(i, new byte[] { (byte) i });
+	}
+
+	@Override
+	public void setDevice(final KnxDevice device)
+	{
+		super.setDevice(device);
+
+		if (device.getAddress().toString().equals("1.1.4"))
+			setProgrammingMode(true);
+		if (device.getAddress().toString().equals("1.1.5"))
+			isResponder.addAll(((DatapointMap<Datapoint>) getDatapointModel()).getDatapoints());
+
+		// the rest here just sets some arbitrary values in interface objects required for testing
+		try {
+			final InterfaceObjectServer ios = device.getInterfaceObjectServer();
+			// set TP1 medium type
+			ios.setDescription(new Description(0, 0, PID.MEDIUM_TYPE, 0, 0, false, 0, 10, 0, 0), true);
+			ios.setProperty(0, PID.MEDIUM_TYPE, 1, 1, new byte[] { 0x1 });
+
+			// set device control property, used for checking verify mode
+			ios.setDescription(new Description(0, 0, PID.DEVICE_CONTROL, 0, 0, true, 0, 10, 0, 0), true);
+			ios.setProperty(0, PID.DEVICE_CONTROL, 1, 1, new byte[] { 0x0 });
+
+			final int last = device.getAddress().getDevice() + 1;
+			final byte[] serialNo = new byte[] { 0x1, 0x2, 0x3, 0x4, 0x5, (byte) last };
+			ios.setProperty(0, PID.SERIAL_NUMBER, 1, 1, serialNo);
+			ios.setDescription(new Description(0, 0, PID.SERIAL_NUMBER, 0, 0, false, 0, 10, 0, 0), true);
+		}
+		catch (final KNXPropertyException e) {
+			e.printStackTrace();
+		}
+
+		// TODO init stuff for KNX IP devices
+		final InterfaceObjectServer ios = device.getInterfaceObjectServer();
+		ios.addInterfaceObject(InterfaceObject.ADDRESSTABLE_OBJECT);
+		ios.addInterfaceObject(InterfaceObject.ASSOCIATIONTABLE_OBJECT);
+		ios.addInterfaceObject(InterfaceObject.INTERFACEPROGRAM_OBJECT);
+		ios.addInterfaceObject(InterfaceObject.KNXNETIP_PARAMETER_OBJECT);
+
+		// create interface object list for property PID_IO_LIST
+		final InterfaceObject[] interfaceObjects = ios.getInterfaceObjects();
+		final int iosize = interfaceObjects.length;
+		final byte[] ioList = new byte[iosize * 2];
+		for (final InterfaceObject io : interfaceObjects) {
+			ioList[io.getIndex() * 2 + 1] = (byte) io.getType();
+		}
+		try {
+			ios.setProperty(0, 1, PID.IO_LIST, 1, iosize, ioList);
+		}
+		catch (final KNXPropertyException e1) {
+			e1.printStackTrace();
+		}
+
+		int idx = 2;
+		setProgramData(ios, idx, (byte) 3);
+		idx = 5;
+		setProgramData(ios, idx, (byte) 5);
+		idx = 3;
+		try {
+			ios.setProperty(idx, PropertyAccess.PID.LOAD_STATE_CONTROL, 1, 1, new byte[] { 1 });
+			idx = 4;
+			ios.setProperty(idx, PropertyAccess.PID.LOAD_STATE_CONTROL, 1, 1, new byte[] { 4 });
+		}
+		catch (final KNXPropertyException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			initKNXnetIpParameterObject(ios, 1, null);
+		}
+		catch (final KNXPropertyException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public DPTXlator requestDatapointValue(final Datapoint dp) throws KNXException
+	{
+		if (!isResponder.contains(dp))
+			return null;
+		final DPTXlator t = TranslatorTypes.createTranslator(0, dp.getDPT());
+		t.setValue(state.get(dp.getMainAddress()));
+		return t;
+	}
+
+	@Override
+	public void updateDatapointValue(final Datapoint ofDp, final DPTXlator update)
+	{
+		System.out.println(device.getAddress() + " -- update " + ofDp.getMainAddress() + " to " + update.getValue());
+		state.put(ofDp.getMainAddress(), update.getValue());
+	}
+
+	@Override
+	public boolean isVerifyModeEnabled()
+	{
+		return false;
+	}
+
+	@Override
+	public ServiceResult readDescriptor(final int type)
+	{
+		return new ServiceResult(new byte[] { 5 << 4 | 7, (byte) (0 << 4 | 5) });
+	}
+
+	@Override
+	public ServiceResult readADC(final int channel, final int consecutiveReads)
+	{
+		return new ServiceResult(new byte[] { (byte) channel, (byte) consecutiveReads, 0x1, 0x0 });
+	}
+
+	@Override
+	public ServiceResult keyWrite(final int accessLevel, final byte[] key)
+	{
+		return new ServiceResult(new byte[] { (byte) accessLevel });
+	}
+
+	@Override
+	public ServiceResult authorize(final byte[] key)
+	{
+		// possible access levels [max .. min]: [0 .. 3] or [0 .. 15]
+		// clients with invalid auth always get minimum level 3/15
+		final int levelInvalidAuth = 15;
+
+		// choose the maximum access level an unauthorized client gets
+		final int maxLevelNoAuth = 14;
+		// the default auth key used for levels providing unauthorized access
+		final byte[] defaultKey = new byte[] { (byte) 0xff, (byte) 0xff, (byte) 0xff, (byte) 0xff };
+
+		final byte[] validKey = new byte[] { 0x10, 0x20, 0x30, 0x40 };
+		final int levelValid = 2;
+
+		int currentLevel = levelInvalidAuth;
+		if (Arrays.equals(key, validKey))
+			currentLevel = levelValid;
+		else if (Arrays.equals(key, defaultKey))
+			currentLevel = maxLevelNoAuth;
+
+		return new ServiceResult(new byte[] { (byte) currentLevel });
+	}
+
+	@Override
+	public ServiceResult restart(final boolean masterReset, final int eraseCode, final int channel)
+	{
+		super.restart(masterReset, eraseCode, channel);
+		if (device.getAddress().equals(new IndividualAddress(1, 1, 4)))
+			setProgrammingMode(true);
+		return null;
+	}
+
+	private static final int NetworkParameterRead = 0b1111011010;
+	private static final int NetworkParameterWrite = 0b1111100100;
+	private static final int NetworkParameterRes = 0b1111011011;
+
+	@Override
+	public ServiceResult management(final int svcType, final byte[] asdu, final KNXAddress dst,
+		final Destination respondTo, final TransportLayer tl)
+	{
+		super.management(svcType, asdu, dst, respondTo, tl);
+		if (svcType == NetworkParameterWrite && LinkProcedure.isEnterConfigMode(asdu)) {
+			final ManagementClientImpl mgmt = new ManagementClientImpl(device.getDeviceLink(), tl) {
+				@Override
+				public KNXNetworkLink detach()
+				{
+					return null;
+				}
+			};
+			final Map<Integer, GroupAddress> groupObjects = new HashMap<>();
+			final int CC_Switch_OnOff = 1;
+			final int CC_Dimming_Ctrl = 5;
+			groupObjects.put(CC_Switch_OnOff, new GroupAddress(7, 3, 10));
+			groupObjects.put(CC_Dimming_Ctrl, new GroupAddress(7, 3, 11));
+			new Thread(LinkProcedure.forSensor(mgmt, device.getAddress(), respondTo, false, 0xbeef, groupObjects),
+					device.getAddress() + " Link Procedure Thread").start();
+		}
+		else if (svcType == NetworkParameterRead) {
+			final int receivedIot = (asdu[0] & 0xff) << 0xff | (asdu[1] & 0xff);
+			final int receivedPid = asdu[2] & 0xff;
+			final byte[] response;
+			final byte[] svcRes = bytesFromWord(NetworkParameterRes);
+			final boolean broadcast = dst.equals(new GroupAddress(0, 0, 0));
+
+			if (receivedIot != 0 || receivedPid != 59) {
+				// if service was sent as broadcast, invalid requests shall be ignored
+				if (broadcast) {
+					System.out.println("invalid A_NetworkParameter.req in broadcast mode, ignore");
+					return null;
+				}
+				response = new byte[] { svcRes[0], svcRes[1], asdu[0], asdu[1], (byte) 0xff };
+				if (receivedIot != 0) {
+					response[2] = (byte) 0xff;
+					response[3] = (byte) 0xff;
+				}
+			}
+			else {
+				response = new byte[2 + asdu.length + 2];
+				response[0] = svcRes[0];
+				response[1] = svcRes[1];
+				response[2] = asdu[0];
+				response[3] = asdu[1];
+				response[4] = asdu[2];
+				response[5] = 1;
+				response[6] = 0xa;
+			}
+			try {
+				System.out.println("A_NetworkParameter.response " + device.getAddress() + "->" + respondTo.getAddress()
+						+ ": " + DataUnitBuilder.toHex(response, ""));
+				final int tmedium = device.getDeviceLink().getKNXMedium().timeFactor();
+				final int wait = broadcast ? new Random().nextInt(10 * tmedium) : 0;
+				System.out.println("add random wait time of " + wait + " ms before response");
+				Thread.sleep(wait);
+				tl.sendData(respondTo.getAddress(), Priority.SYSTEM, response);
+			}
+			catch (KNXLinkClosedException | KNXTimeoutException | InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	// precondition: we have an IOS instance
+	private void initKNXnetIpParameterObject(final InterfaceObjectServer ios, final int objectInstance,
+		final RoutingEndpoint endpoint) throws KNXPropertyException
+	{
+		final int knxObject = 11;
+		// reset transmit counter to 0
+		// those two are 4 byte unsigned
+		ios.setProperty(knxObject, objectInstance, PID.MSG_TRANSMIT_TO_IP, 1, 1, new byte[4]);
+		ios.setProperty(knxObject, objectInstance, PID.MSG_TRANSMIT_TO_KNX, 1, 1, new byte[4]);
+
+		//
+		// set properties used in device DIB for search response during discovery
+		//
+		// friendly name property entry is an array of 30 characters
+		final byte[] data = new byte[30];
+		System.arraycopy(friendlyName.getBytes(), 0, data, 0, friendlyName.length());
+		ios.setProperty(knxObject, objectInstance, PID.FRIENDLY_NAME, 1, data.length, data);
+		ios.setProperty(knxObject, objectInstance, PID.PROJECT_INSTALLATION_ID, 1, 1,
+				bytesFromWord(defProjectInstallationId));
+		// server KNX device address, since we don't know about routing at this time
+		// address is always 0.0.0, but is updated in setRoutingConfiguration
+		final byte[] device = new IndividualAddress(0).toByteArray();
+		ios.setProperty(knxObject, objectInstance, PID.KNX_INDIVIDUAL_ADDRESS, 1, 1, device);
+		ios.setProperty(knxObject, objectInstance, PID.MAC_ADDRESS, 1, 1, defMacAddress);
+
+		// routing stuff
+		if (endpoint != null)
+			;//	setRoutingConfiguration(endpoint);
+		else
+			resetRoutingConfiguration(ios);
+
+		// ip and setup multicast
+		byte[] ip = new byte[4];
+		try {
+			ip = InetAddress.getLocalHost().getAddress();
+		}
+		catch (final UnknownHostException e) {}
+		ios.setProperty(knxObject, objectInstance, PID.CURRENT_IP_ADDRESS, 1, 1, ip);
+		ip[3] = (byte) (ip[3] - 1);
+		ios.setProperty(knxObject, objectInstance, PID.IP_ADDRESS, 1, 1, ip);
+		ios.setProperty(knxObject, objectInstance, PID.SUBNET_MASK, 1, 1, new byte[] { -1, -1, -1, 0 });
+		ios.setProperty(knxObject, objectInstance, PID.DEFAULT_GATEWAY, 1, 1, ip);
+		ios.setProperty(knxObject, objectInstance, PID.TTL, 1, 1, new byte[] { 9 });
+
+		try {
+			final byte[] a1 = new IndividualAddress("1.1.5").toByteArray();
+			final byte[] a2 = new IndividualAddress("1.1.6").toByteArray();
+			final byte[] a3 = new IndividualAddress("1.1.7").toByteArray();
+			ios.setProperty(knxObject, objectInstance, PID.ADDITIONAL_INDIVIDUAL_ADDRESSES, 1, 1, a1);
+			ios.setProperty(knxObject, objectInstance, PID.ADDITIONAL_INDIVIDUAL_ADDRESSES, 2, 1, a2);
+			ios.setProperty(knxObject, objectInstance, PID.ADDITIONAL_INDIVIDUAL_ADDRESSES, 3, 1, a3);
+		}
+		catch (final KNXFormatException e) {
+			e.printStackTrace();
+		}
+
+		ios.setProperty(knxObject, objectInstance, PID.SYSTEM_SETUP_MULTICAST_ADDRESS, 1, 1,
+				defRoutingMulticast.getAddress());
+
+		//
+		// set properties used in service families DIB for description
+		//
+		ios.setProperty(knxObject, objectInstance, PID.KNXNETIP_DEVICE_CAPABILITIES, 1, 1,
+				bytesFromWord(defDeviceCaps));
+
+		//
+		// set properties used in manufacturer data DIB for discovery self description
+		//
+		final byte[] zero = new byte[1];
+		// we don't indicate any capabilities here, since executing the respective tasks
+		// is either done in the gateway (and, therefore, the property is set by the
+		// gateway) or by the user, who has to care about it on its own
+		ios.setProperty(knxObject, objectInstance, PID.KNXNETIP_ROUTING_CAPABILITIES, 1, 1, zero);
+		ios.setProperty(knxObject, objectInstance, PID.KNXNETIP_DEVICE_STATE, 1, 1, zero);
+
+		ios.setProperty(knxObject, objectInstance, PID.IP_CAPABILITIES, 1, 1, zero);
+		ios.setProperty(knxObject, objectInstance, PID.IP_ASSIGNMENT_METHOD, 1, 1, new byte[] { 1 });
+		ios.setProperty(knxObject, objectInstance, PID.CURRENT_IP_ASSIGNMENT_METHOD, 1, 1, new byte[] { 1 });
+	}
+
+	private void resetRoutingConfiguration(final InterfaceObjectServer ios)
+	{
+		// routing multicast shall be set 0 if no routing service offered
+		try {
+			ios.setProperty(InterfaceObject.KNXNETIP_PARAMETER_OBJECT, 1, PID.ROUTING_MULTICAST_ADDRESS, 1, 1,
+					new byte[4]);
+		}
+		catch (final KNXPropertyException e) {}
+	}
+
+	private void setProgramData(final InterfaceObjectServer ios, final int idx, final byte value)
+	{
+		try {
+			ios.setProperty(idx, PropertyAccess.PID.PROGRAM_VERSION, 1, 1, new byte[] { value });
+			ios.setProperty(idx, PropertyAccess.PID.LOAD_STATE_CONTROL, 1, 1, new byte[] { value });
+			ios.setProperty(idx, PropertyAccess.PID.RUN_STATE_CONTROL, 1, 1, new byte[] { value });
+			ios.setProperty(idx, PropertyAccess.PID.ERROR_CODE, 1, 1, new byte[] { 8 });
+		}
+		catch (final KNXPropertyException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static byte[] bytesFromWord(final int word)
+	{
+		return new byte[] { (byte) (word >> 8), (byte) word };
+	}
+}
---
+++ src/main/java/tuwien/auto/calimero/TestNetwork.java
@@ -0,0 +1,135 @@
+/*
+    Calimero 2 - A library for KNX network access
+    Copyright (c) 2010, 2016 B. Malinowsky
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Linking this library statically or dynamically with other modules is
+    making a combined work based on this library. Thus, the terms and
+    conditions of the GNU General Public License cover the whole
+    combination.
+
+    As a special exception, the copyright holders of this library give you
+    permission to link this library with independent modules to produce an
+    executable, regardless of the license terms of these independent
+    modules, and to copy and distribute the resulting executable under terms
+    of your choice, provided that you also meet, for each linked independent
+    module, the terms and conditions of the license of that module. An
+    independent module is a module which is not derived from or based on
+    this library. If you modify this library, you may extend this exception
+    to your version of the library, but you are not obligated to do so. If
+    you do not wish to do so, delete this exception statement from your
+    version.
+*/
+
+package tuwien.auto.calimero;
+
+import java.util.List;
+
+import tuwien.auto.calimero.device.BaseKnxDevice;
+import tuwien.auto.calimero.device.KnxDevice;
+import tuwien.auto.calimero.link.KNXNetworkLink;
+import tuwien.auto.calimero.process.ProcessCommunicationBase;
+import tuwien.auto.calimero.process.ProcessCommunicator;
+import tuwien.auto.calimero.process.ProcessCommunicatorImpl;
+import tuwien.auto.calimero.server.Launcher;
+import tuwien.auto.calimero.server.VirtualLink;
+import tuwien.auto.calimero.server.gateway.SubnetConnector;
+
+public class TestNetwork implements Runnable
+{
+	// loop interval [ms] of issuing read/write datapoint requests to keep a "live" network
+	private static final int UpdateInterval = 10000;
+
+	private final String configURI;
+
+	/**
+	 * @param args server config URI
+	 */
+	public static void main(final String[] args)
+	{
+		if (args.length == 0)
+			System.err.println("supply calimero-server configuration -- exit");
+		else
+			new TestNetwork(args).run();
+	}
+
+	public TestNetwork(final String[] args)
+	{
+		configURI = args[0];
+	}
+
+	@Override
+	public void run()
+	{
+		System.getProperties().setProperty("org.slf4j.simpleLogger.logFile", "System.out");
+		System.getProperties().setProperty("org.slf4j.simpleLogger.showLogName", "true");
+
+		try {
+			final Launcher svr = new Launcher(configURI);
+			new Thread(svr::run).start();
+			Thread.sleep(1000);
+
+			final List<SubnetConnector> connectors = svr.getGateway().getSubnetConnectors();
+			final VirtualLink link = (VirtualLink) connectors.get(0).getSubnetLink();
+
+			final KnxDevice d4 = createDevice("1.1.4", link);
+			/*final KnxDevice d5 =*/ createDevice("1.1.5", link);
+			System.out.println("Test network is up and running");
+
+			boolean state = true;
+			int intState = 13;
+			try (ProcessCommunicator pc = new ProcessCommunicatorImpl(d4.getDeviceLink())) {
+				while (true) {
+					Thread.sleep(UpdateInterval);
+					final boolean createReadWriteTraffic = true;
+					if (createReadWriteTraffic) {
+						try {
+							state = !state;
+							pc.write(new GroupAddress("1/0/1"), state);
+							pc.readBool(new GroupAddress("1/0/1"));
+						}
+						catch (final KNXException e) {
+							System.out.println(e);
+						}
+
+						try {
+							intState = ++intState % 101;
+							pc.write(new GroupAddress("1/0/3"), intState, ProcessCommunicationBase.SCALING);
+							pc.readUnsigned(new GroupAddress("1/0/3"), ProcessCommunicationBase.SCALING);
+						}
+						catch (final KNXException e) {
+							System.out.println(e);
+						}
+					}
+				}
+			}
+		}
+		catch (final KNXException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static KnxDevice createDevice(final String address, final VirtualLink downLink) throws KNXException
+	{
+		final IndividualAddress ia = new IndividualAddress(address);
+		final TestDeviceLogic logic = new TestDeviceLogic();
+		final KNXNetworkLink devLink = downLink.createDeviceLink(ia);
+		final KnxDevice dev = new BaseKnxDevice("Device " + ia.toString(), ia, devLink, logic, logic);
+		// XXX we have to init device stuff in setDevice, because before the device is null
+		logic.setDevice(dev);
+		return dev;
+	}
+}
