ac90768cc89763be7a634cdd441b306b34204289
bmalinowsky <bmalinowsky@github.com>
2025-10-08 18:06:53 +0200

    Remove unused interface object

--- src/main/java/io/calimero/testnetwork/TestDeviceLogic.java
+++ src/main/java/io/calimero/testnetwork/TestDeviceLogic.java
@@ -39,11 +39,6 @@ package io.calimero.testnetwork;
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 import java.lang.invoke.MethodHandles;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -56,7 +51,6 @@ import io.calimero.GroupAddress;
 import io.calimero.IndividualAddress;
 import io.calimero.KNXAddress;
 import io.calimero.KNXException;
-import io.calimero.KNXFormatException;
 import io.calimero.ReturnCode;
 import io.calimero.datapoint.Datapoint;
 import io.calimero.datapoint.DatapointMap;
@@ -83,7 +77,6 @@ import io.calimero.dptxlator.DptXlator16BitSet;
 import io.calimero.dptxlator.PropertyTypes;
 import io.calimero.dptxlator.TranslatorTypes;
 import io.calimero.internal.Executor;
-import io.calimero.knxnetip.KNXnetIPRouting;
 import io.calimero.link.medium.RFSettings;
 import io.calimero.log.LogService;
 import io.calimero.mgmt.Description;
@@ -100,32 +93,6 @@ import io.calimero.mgmt.TransportLayer;
 class TestDeviceLogic extends KnxDeviceServiceLogic
 {
 	private static final Logger logger = LogService.getLogger(MethodHandles.lookup().lookupClass());
-
-	// PID.PROJECT_INSTALLATION_ID
-	private static final int defProjectInstallationId = 0;
-	// PID.ROUTING_MULTICAST_ADDRESS
-	private static final InetAddress defRoutingMulticast = KNXnetIPRouting.DefaultMulticast;
-	// PID.MAC_ADDRESS
-	private static final byte[] defMacAddress;
-
-	static {
-		byte[] mac = null;
-		try {
-			mac = NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress();
-		}
-		catch (SocketException | UnknownHostException | NullPointerException ignore) {}
-		defMacAddress = mac != null ? mac : new byte[6];
-	}
-
-	// Values used for service families DIB
-
-	// PID.KNXNETIP_DEVICE_CAPABILITIES
-	// Bits LSB to MSB: 0 Device Management, 1 Tunneling, 2 Routing, 3 Remote Logging,
-	// 4 Remote Configuration and Diagnosis, 5 Object Server
-	private static final int defDeviceCaps = 1 + 2 + 4;
-
-	// server friendly name, matches PID.FRIENDLY_NAME property
-	private static final String friendlyName = "KNX Test Device";
 
 	private static final int pidOperationMode = 52;
 
@@ -217,8 +184,6 @@ class TestDeviceLogic extends KnxDeviceServiceLogic
 			e.printStackTrace();
 		}
 
-		ios.addInterfaceObject(InterfaceObject.KNXNETIP_PARAMETER_OBJECT);
-
 		// create interface object list for property PID_IO_LIST
 		final InterfaceObject[] interfaceObjects = ios.getInterfaceObjects();
 		final int iosize = interfaceObjects.length;
@@ -251,13 +216,6 @@ class TestDeviceLogic extends KnxDeviceServiceLogic
 		ios.setDescription(new Description(appProg.getIndex(), 0, pidOperationMode, 0,
 				PropertyTypes.PDT_FUNCTION, true, 1, 1, 3, 3), true);
 		ios.setProperty(InterfaceObject.APPLICATIONPROGRAM_OBJECT, 1, pidOperationMode, 1, 1, (byte) 0);
-
-		try {
-			initKNXnetIpParameterObject(ios, 1);
-		}
-		catch (final KnxPropertyException e) {
-			e.printStackTrace();
-		}
 	}
 
 	@Override
@@ -402,95 +360,6 @@ class TestDeviceLogic extends KnxDeviceServiceLogic
 		return 0;
 	}
 
-	// precondition: we have an IOS instance
-	private static void initKNXnetIpParameterObject(final InterfaceObjectServer ios, final int objectInstance)
-		throws KnxPropertyException
-	{
-		final int knxObject = 11;
-		// reset transmit counter to 0
-		// those two are 4 byte unsigned
-		ios.setProperty(knxObject, objectInstance, PID.MSG_TRANSMIT_TO_IP, 1, 1, new byte[4]);
-		ios.setProperty(knxObject, objectInstance, PID.MSG_TRANSMIT_TO_KNX, 1, 1, new byte[4]);
-
-		//
-		// set properties used in device DIB for search response during discovery
-		//
-		// friendly name property entry is an array of 30 characters
-		final byte[] data = new byte[30];
-		System.arraycopy(friendlyName.getBytes(StandardCharsets.ISO_8859_1), 0, data, 0, friendlyName.length());
-		ios.setProperty(knxObject, objectInstance, PID.FRIENDLY_NAME, 1, data.length, data);
-		ios.setProperty(knxObject, objectInstance, PID.PROJECT_INSTALLATION_ID, 1, 1,
-				bytesFromWord(defProjectInstallationId));
-		// server KNX device address, since we don't know about routing at this time
-		// address is always 0.0.0, but is updated in setRoutingConfiguration
-		final byte[] device = new IndividualAddress(0).toByteArray();
-		ios.setProperty(knxObject, objectInstance, PID.KNX_INDIVIDUAL_ADDRESS, 1, 1, device);
-		ios.setProperty(knxObject, objectInstance, PID.MAC_ADDRESS, 1, 1, defMacAddress);
-
-		// routing stuff
-		resetRoutingConfiguration(ios);
-
-		// ip and setup multicast
-		byte[] ip = new byte[4];
-		try {
-			ip = InetAddress.getLocalHost().getAddress();
-		}
-		catch (final UnknownHostException e) {}
-		ios.setProperty(knxObject, objectInstance, PID.CURRENT_IP_ADDRESS, 1, 1, ip);
-		ip[3] = (byte) (ip[3] - 1);
-		ios.setProperty(knxObject, objectInstance, PID.IP_ADDRESS, 1, 1, ip);
-		ios.setProperty(knxObject, objectInstance, PID.SUBNET_MASK, 1, 1, new byte[] { -1, -1, -1, 0 });
-		ios.setProperty(knxObject, objectInstance, PID.DEFAULT_GATEWAY, 1, 1, ip);
-		ios.setProperty(knxObject, objectInstance, PID.TTL, 1, 1, new byte[] { 9 });
-
-		try {
-			final byte[] a1 = new IndividualAddress("1.1.5").toByteArray();
-			final byte[] a2 = new IndividualAddress("1.1.6").toByteArray();
-			final byte[] a3 = new IndividualAddress("1.1.7").toByteArray();
-			final int idx = ios.getProperty(knxObject, 1, PID.OBJECT_INDEX, 1, 1)[0];
-			ios.setDescription(new Description(idx, knxObject, PID.ADDITIONAL_INDIVIDUAL_ADDRESSES, 0, 0, true, 0, 10, 3, 3), true);
-			ios.setProperty(knxObject, objectInstance, PID.ADDITIONAL_INDIVIDUAL_ADDRESSES, 1, 1, a1);
-			ios.setProperty(knxObject, objectInstance, PID.ADDITIONAL_INDIVIDUAL_ADDRESSES, 2, 1, a2);
-			ios.setProperty(knxObject, objectInstance, PID.ADDITIONAL_INDIVIDUAL_ADDRESSES, 3, 1, a3);
-		}
-		catch (final KNXFormatException e) {
-			e.printStackTrace();
-		}
-
-		ios.setProperty(knxObject, objectInstance, PID.SYSTEM_SETUP_MULTICAST_ADDRESS, 1, 1,
-				defRoutingMulticast.getAddress());
-
-		//
-		// set properties used in service families DIB for description
-		//
-		ios.setProperty(knxObject, objectInstance, PID.KNXNETIP_DEVICE_CAPABILITIES, 1, 1,
-				bytesFromWord(defDeviceCaps));
-
-		//
-		// set properties used in manufacturer data DIB for discovery self-description
-		//
-
-		// we don't indicate any capabilities here, since executing the respective tasks
-		// is either done in the gateway (and, therefore, the property is set by the
-		// gateway) or by the user, who has to care about it on its own
-		ios.setProperty(knxObject, objectInstance, PID.KNXNETIP_ROUTING_CAPABILITIES, 1, 1, (byte) 0);
-		ios.setProperty(knxObject, objectInstance, PID.KNXNETIP_DEVICE_STATE, 1, 1, (byte) 0);
-
-		ios.setProperty(knxObject, objectInstance, PID.IP_CAPABILITIES, 1, 1, (byte) 0);
-		ios.setProperty(knxObject, objectInstance, PID.IP_ASSIGNMENT_METHOD, 1, 1, (byte) 1);
-		ios.setProperty(knxObject, objectInstance, PID.CURRENT_IP_ASSIGNMENT_METHOD, 1, 1, (byte) 1);
-	}
-
-	private static void resetRoutingConfiguration(final InterfaceObjectServer ios)
-	{
-		// routing multicast shall be set 0 if no routing service offered
-		try {
-			ios.setProperty(InterfaceObject.KNXNETIP_PARAMETER_OBJECT, 1, PID.ROUTING_MULTICAST_ADDRESS, 1, 1,
-					new byte[4]);
-		}
-		catch (final KnxPropertyException e) {}
-	}
-
 	private static void setProgramData(final InterfaceObjectServer ios, final int idx, final byte value)
 	{
 		try {
@@ -502,10 +371,5 @@ class TestDeviceLogic extends KnxDeviceServiceLogic
 		catch (final KnxPropertyException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private static byte[] bytesFromWord(final int word)
-	{
-		return new byte[] { (byte) (word >> 8), (byte) word };
 	}
 }
